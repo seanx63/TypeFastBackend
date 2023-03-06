0242b373cb786775e5f243ea54f93fb3af8b7fdc
Sean Gates <seanx63@gmail.com>
2023-03-06 07:16:48 -0700

    add properties and example user

--- src/main/java/seanx63/TypeFastBackend/TypeFastBackendApplication.java
+++ src/main/java/seanx63/TypeFastBackend/TypeFastBackendApplication.java
@@ -1,13 +1,26 @@
 package seanx63.TypeFastBackend;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import seanx63.TypeFastBackend.domain.User;
+import seanx63.TypeFastBackend.repository.UserRepository;
 
 @SpringBootApplication
-public class TypeFastBackendApplication {
+public class TypeFastBackendApplication implements CommandLineRunner {
+
+	private final UserRepository userRepository;
+
+	public TypeFastBackendApplication(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TypeFastBackendApplication.class, args);
 	}
 
+	@Override
+	public void run(String... strings) throws Exception {
+		this.userRepository.save(new User("user", "pass"));
+	}
 }
